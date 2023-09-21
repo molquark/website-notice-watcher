3b570c1bb26729dcdcb862d533c50cee83ea7e8c
LDY <ldy1456714026@outlook.com>
2023-09-21 13:14:44 +0800

    对main.py各步骤进行封装, 准备添加参数解析

--- main.py
+++ main.py
@@ -13,34 +13,44 @@ XPath解析网页
 
 import sys
 from email.mime.text import MIMEText  # 导入email库的MIMEText类，用于创建邮件内容
+
+from TargetConfig import TargetConfig
 from EmailSender import EmailSender
 from RunTimeContext import RunTimeContext
 from WebWatcher import WebWatcher
 
+# 全局变量
+runtimeContext: RunTimeContext
 
-if __name__ == "__main__":
-    configFilePath = sys.argv[1]
 
-    runtimeContext = RunTimeContext(configFilePath)
-
-    targets = runtimeContext.get_targets()
-    targetConfigs = runtimeContext.get_target_configs()
-    observers = runtimeContext.get_observers()
-
-    # 读取网页
+# 封装后的函数
+def get_targetResult(target_configs: dict[str, TargetConfig]) -> dict[str, list[WebWatcher.WatchResult]]:
     targetResults: dict[str, list[WebWatcher.WatchResult]] = {}
-    for target, targetConfig in targetConfigs.items():
+    for target, targetConfig in target_configs.items():
         watcher = WebWatcher(targetConfig, runtimeContext)
         targetResults[target] = watcher.webWatch(runtimeContext.get_last_run_time())
+    return targetResults
 
-    # TODO: 格式化发送内容
+
+def get_targetMsgs(targetResults: dict[str, list[WebWatcher.WatchResult]]) -> dict[str, MIMEText]:
     targetMsgs: dict[str, MIMEText] = {}
     for target, results in targetResults.items():
         if len(results) > 0:  # 有内容才发送
             targetMsgs[target] = MIMEText("\n".join(map(lambda x: str(x), results)), "plain", "utf-8")
+    return targetMsgs
 
-    # TODO: 发送邮件
+
+def send_email(targetMsgs: dict[str, MIMEText]):
+    """
+    根据配置发送邮件
+
+    :return: 无
+    """
+    # 从全局运行上下文中获取数据
+    targetConfigs = runtimeContext.get_target_configs()
+    observers = runtimeContext.get_observers()
     emailSender = EmailSender(runtimeContext.get_email_config())
+
     if len(sys.argv) == 3 and sys.argv[2] == "test":  # 测试模式
         for target, msg in targetMsgs.items():
             print(bytes(msg.get_payload(decode=True)).decode(encoding="utf8"), observers[target],
@@ -49,8 +59,37 @@ if __name__ == "__main__":
         for target, msg in targetMsgs.items():  # 正常模式
             emailSender.sendEmail(msg, observers[target], Subject="新公告通知 " + targetConfigs[target].targetName)
 
+
+def before_exit():
+    """
+    退出脚本前的处理, 包括本次运行时间的保存等...
+
+    :return: 无
+    """
     # 保存本次运行时间
     runtimeContext.dump_last_record()
+
+
+
+if __name__ == "__main__":
+    configFilePath = sys.argv[1]
+
+    runtimeContext = RunTimeContext(configFilePath)
+
+    targets = runtimeContext.get_targets()
+
+
+    # 读取网页
+    targetResults = get_targetResult(target_configs=runtimeContext.get_target_configs())
+
+    # TODO: 格式化发送内容
+    targetMsgs = get_targetMsgs(targetResults=targetResults)
+
+    # TODO: 发送邮件
+    send_email(targetMsgs=targetMsgs)
+
+    # 脚本退出前处理
+    before_exit()
 
 
 
